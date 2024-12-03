96c336a10049eb68e814e656fd0c2f000a7b326b
Fabrizio Calderan <fcalderan@users.noreply.github.com>
2024-12-03 17:01:36 +0100

    Refactoring code
    Scraper class included as a separate file

--- rub2eur-X.py
+++ rub2eur-X.py
@@ -1,18 +1,22 @@
 # Copyright (c) 2024 – today by Fabrizo Calderan. All rights reserved.
 # This code is licensed under a non-commercial license.
 # You may not use, copy, modify, or distribute this code for commercial purposes.
-
-
+#
+#
 # ======================================================
-# Imports
+#  Imports
 # ======================================================
+import warnings
+warnings.filterwarnings("ignore", module="urllib3")
+
 import time
 import datetime
-import pytz     
-import tweepy   
-import schedule 
-from io import BytesIO
-from playwright.sync_api import sync_playwright 
+import pytz     # type: ignore
+import tweepy   # type: ignore
+import schedule # type: ignore
+import os
+from utils.exchangeRateScraper import ExchangeRateScraper # type: ignore
+from dotenv import load_dotenv # type: ignore
 
 
 
@@ -20,14 +24,14 @@ from playwright.sync_api import sync_playwright
 # Configuration
 # ======================================================
 
-PROD_MODE = True
-URL = "https://www.google.com/finance/quote/RUB-EUR?window=5D"
+PROD_MODE = False
 
-API_KEY = "..."
-API_SECRET = "..."
-ACCESS_TOKEN = "..."
-ACCESS_SECRET = "..."
-BEARER_TOKEN = "..."
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+API_SECRET = os.getenv("API_SECRET")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_SECRET = os.getenv("ACCESS_SECRET")
+BEARER_TOKEN = os.getenv("BEARER_TOKEN")
 OUTPUT_RATE = 1.75 if (PROD_MODE) else 0.0015
 
 
@@ -57,44 +61,6 @@ class TwitterBot:
 
 
 
-class ExchangeRateScraper:
-
-    def get_exchange_data(self):
-        with sync_playwright() as p:
-            browser = p.chromium.launch(headless=True)
-            context = browser.new_context(
-                extra_http_headers={
-                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-                    "Accept-Language": "en-US,en;q=0.9",
-                    "Accept-Encoding": "gzip, deflate, br",
-                    "Connection": "keep-alive",
-                    "sec-ch-ua": "Not A(Brand';v='99', 'Google Chrome';v='121', 'Chromium';v='121'",
-                    "referer": "https://www.google.com/"
-                }
-            )
-            page = context.new_page()
-            page.goto(URL)
-
-            try:
-                page.wait_for_selector("span:has-text('Accept all')", timeout=5000)
-                page.click("span:has-text('Accept all')")
-            except Exception:
-                print("No consent dialog found.")
-
-            page.wait_for_load_state("networkidle")
-            page.wait_for_selector('[data-last-price]', timeout=5000)
-
-            rate = float(page.query_selector('[data-last-price]').get_attribute('data-last-price'))
-            trend = page.query_selector('[data-last-price] span[aria-label]').get_attribute('aria-label').lower()
-            chart = page.query_selector('div[aria-hidden][jslog]')
-            chart = chart.screenshot(path=None)
-            chart = BytesIO(chart)
-            browser.close()
-
-            return {"rate": rate, "trend": trend, "chart": chart}
-
-
-
 class ExchangeRateBot:
     
     def __init__(self, scraper, twitter_bot):
@@ -107,7 +73,7 @@ class ExchangeRateBot:
         ts = datetime.datetime.now(tz)
         timestamp = ts.strftime("%d/%m/%y %H:%M")
     
-        data = self.scraper.get_exchange_data()
+        data  = self.scraper.get_exchange_data()
         rate, trend, chart = data["rate"], data["trend"], data["chart"]
 
         alt_text = f"#EURRUB exchange rate: {rate:.4f}, as of {timestamp} " 
@@ -124,7 +90,7 @@ class ExchangeRateBot:
         if PROD_MODE:
             self.twitter_bot.tweet(tweet, chart, alt_text)
 
-        print(f"[{timestamp}] Tweet Content:\n—————— \n{tweet}\n——————\n\n")
+        print(f"[{timestamp}] Tweet Content:\n—————— \n{tweet}\n——————\n")
 
         
 
